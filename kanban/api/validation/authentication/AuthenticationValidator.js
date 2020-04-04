c82aece29764255c469054501aa02daed93d11b5
Michał Gębicki <gebicki.michal98@gmail.com>
2020-04-04 20:29:44 +0200

    add sign up validator

---
+++ kanban/api/validation/authentication/AuthenticationValidator.js
@@ -0,0 +1,66 @@
+const userConfig = require('../../config/user')
+const ValidatorAbstract = require('../ValidatorAbstract')
+
+class AuthenticationValidator extends ValidatorAbstract {
+    /**
+     * Validate sign up request
+     * 
+     * @param {Object} payload
+     * @return {void} 
+     */
+    static validateSignUpRequest(payload) {
+        if (payload.username == null || payload.username == "") {
+            throw new Error('Username is required')
+        }
+        
+        const usernameLength = payload.username.length
+
+        if (usernameLength < userConfig.validation.minUsernameLength || usernameLength > userConfig.validation.maxUsernameLength) {
+            throw new Error('Username length is invalid')
+        }
+
+        if (payload.pin == null || payload.pin == "") {
+            throw new Error('PIN is required')
+        }
+
+        const pinLength = payload.pin.length
+
+        if (pinLength != userConfig.validation.pinLength) {
+            throw new Error('Invalid PIN length')
+        }
+
+        if (isNaN(payload.pin)) {
+            throw new Error('PIN should be a number')
+        }
+
+        if (payload.first_name == null || payload.first_name == "") {
+            throw new Error('First name is required')
+        }
+
+        const firstNameLength = payload.first_name.length
+
+        if (firstNameLength > userConfig.validation.maxFirstNameLength || firstNameLength < userConfig.validation.minFirstNameLength) {
+            throw new Error('Invalid first name length')
+        }
+
+        if (payload.last_name == null || payload.last_name == "") {
+            throw new Error('Last name is required')
+        }
+
+        const lastNameLength = payload.last_name.length
+
+        if (lastNameLength > userConfig.validation.maxLastNameLength || lastNameLength < userConfig.validation.minLastNameLength) {
+            throw new Error('Invalid last name length')
+        }
+
+        const userType = payload.user_type
+
+        if (userType == null || userType == "") {
+            throw new Error('User type is required')
+        }
+
+        return
+    }
+}
+
+module.exports = AuthenticationValidator
