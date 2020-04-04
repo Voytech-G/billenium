31ba2d36fe22fc215523767fc318767819a4f0bc
Michał Gębicki <gebicki.michal98@gmail.com>
2020-04-04 20:24:45 +0200

    add user config and model

---
+++ kanban/api/config/user.js
@@ -0,0 +1,14 @@
+module.exports = {
+    validation: {
+        pinLength: 4,
+        maxUsernameLength: 12,
+        minUsernameLength: 3,
+        maxFirstNameLength: 15,
+        minFirstNameLength: 2,
+        maxLastNameLength: 15,
+        minLastNameLength: 2,
+    },
+    authentication: {
+        saltRounds: 10,
+    },
+}
