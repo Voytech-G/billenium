9b8a5d6a3825c32d326a4fa0e6b98e5ef54a736e
Voytech <wojciechgwardjak@gmail.com>
2020-04-13 18:11:58 +0200

    test

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -9,23 +9,28 @@ const App = () => {
   const { setColumns, addCard } = useContext(GlobalContext);
 
   useEffect(() => {
-    socket.emit("get-project", {
-      project_id: '5e877170c2386013906d7421',
-    }, data => {
-      const columnsWithItems = data.payload.columns.map(column => ({
-        id: column._id,
-        name: column.name,
-        board_index: column.board_index,
-        max_tasks: column.max_tasks,
-        items: column.tasks.map(task => ({
-          id: task._id,
-          content: task.content,
-          row_index: task.row_index
-        }))
-      }));
-      // console.log(columnsWithItems);
-      setColumns(columnsWithItems);
-    });
+    console.log("witam");
+    socket.emit(
+      "get-project",
+      {
+        project_id: "5e877170c2386013906d7421",
+      },
+      (data) => {
+        const columnsWithItems = data.payload.columns.map((column) => ({
+          id: column._id,
+          name: column.name,
+          board_index: column.board_index,
+          max_tasks: column.max_tasks,
+          items: column.tasks.map((task) => ({
+            id: task._id,
+            content: task.content,
+            row_index: task.row_index,
+          })),
+        }));
+        // console.log(columnsWithItems);
+        setColumns(columnsWithItems);
+      }
+    );
   }, []);
 
   return (
