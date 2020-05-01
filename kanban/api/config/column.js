86cdbc1c7740c87ee954bb8113149722754bfd6d
Michał Gębicki <gebicki.michal98@gmail.com>
2020-05-01 14:46:43 +0200

    unassign subproject from tasks after the subprojects are removed

--- kanban/api/config/column.js
+++ kanban/api/config/column.js
@@ -3,5 +3,5 @@ module.exports = {
         RETURN_NEW_AFTER_UPDATE: true,
         USE_FIND_AND_MODIFY: true,
     },
-    REMOVE_TASKS_ON_COLUMN_DELETE: false,
+    REMOVE_TASKS_ON_COLUMN_DELETE: true,
 }
